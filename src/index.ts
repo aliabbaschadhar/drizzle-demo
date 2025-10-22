93bb338181e467234819d989116a50732dc1a9b9
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-10-22 21:12:16 +0500

    db client created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(" via !");
+import { db } from "./drizzle/db";
+import { UserTable } from "./drizzle/schema";
+
+async function main() {
+  const user = await db.insert(UserTable).values({
+    name: "Shaka-g",
+  }).returning();
+  // const user = await db.query.UserTable.findFirst();
+  console.log("New created user is:", user)
+}
+
+main();
