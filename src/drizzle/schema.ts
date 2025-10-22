7dd290fb0a7de1f4c2f9ddd29b9a2da319bc0be7
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-10-22 07:44:47 +0500

    update:drizzle.config.ts and schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,7 +1,5 @@
 import { serial } from "drizzle-orm/mysql-core";
 import { integer, pgTable, uuid, varchar, } from "drizzle-orm/pg-core";
-import { drizzle } from "drizzle-orm/postgres-js";
-const db = drizzle(process.env.DATABASE_URL as string);
 
 export const UserTable = pgTable("users", {
   // id: serial("id").primaryKey(), // Id as primary which  auto increments one by one
