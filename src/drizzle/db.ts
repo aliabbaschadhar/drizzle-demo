5e361223028cc3d692c4a0040b5f506b73b65e38
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-10-28 02:19:53 +0500

    onConflictDoUpdate only works if the insert in diffferent batches

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 const client = postgres(process.env.DATABASE_URL as string, {
-  prepare: false
+  prepare: false // Disable prepared statements for simplicity
 });
 
 export const db = drizzle(client, { schema, logger: true })
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,56 @@ import { db } from "./drizzle/db";
 import { UserTable } from "./drizzle/schema";
 
 async function main() {
-  const user = await db.insert(UserTable).values({
+  await db.delete(UserTable)
+  const user = await db.insert(UserTable).values([{
     name: "Shaka-g",
-  }).returning();
+    age: 22,
+    email: `${Math.random().toString(36).substring(2, 15)}@example.com`,
+    role: "BASIC"
+  }, {
+    name: "Ali",
+    age: 22,
+    email: `${Math.random().toString(36).substring(2, 15)}@example.com`,
+    role: "ADMIN"
+  }, {
+    name: "Ali Abbas",
+    age: 23,
+    email: `${Math.random().toString(36).substring(2, 15)}@example.com`,
+    role: "BASIC"
+  }]).returning({
+    id: UserTable.id,
+    name: UserTable.name,
+    email: UserTable.email,
+    role: UserTable.role
+  }).onConflictDoUpdate({
+    target: [UserTable.age, UserTable.name],
+    set: {
+      name: `Updated Name + ${Math.floor(Math.random() * 100)}`,
+      age: 23 + Math.floor(Math.random() * 10),
+    }
+  })
   // const user = await db.query.UserTable.findFirst();
   console.log("New created user is:", user)
+
+  const updatedUser = await db.insert(UserTable).values({
+    name: "Ali",
+    age: 22,
+    email: "hello@gmail.com",
+    role: "BASIC"
+  }).returning({
+    id: UserTable.id,
+    name: UserTable.name,
+    age: UserTable.age
+  }).onConflictDoUpdate({
+    target: [UserTable.age, UserTable.name],
+    set: {
+      age: 60,
+      name: "Updated Name"
+    }
+  })
+  console.log("Updated user:", updatedUser)
+
+
 }
 
 main();
